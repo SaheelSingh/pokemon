37a8290501c98ad05301ce8b192560c5c7f89843
SaheelSingh <saheelsingh1904@gmail.com>
2024-08-13 14:37:32 +0530

    added heading to pokemon list

--- src/component/PokemonList.js
+++ src/component/PokemonList.js
@@ -3,6 +3,7 @@ import React from 'react'
 function PokemonList({ pokemon }) {
   return (
     <div>
+      <h1>Pokemon List</h1>
       {
         pokemon.map(p => (
             <div key={p}>{p}</div>
